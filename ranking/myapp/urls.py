0c4d4b01ff833dcb575f9d362c612153e66085f8
Alan <bednarczykalan@gmail.com>
2024-01-25 23:39:18 +0100

    change urls

--- ranking/myapp/urls.py
+++ ranking/myapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import person_list, delete_person, add_person, UpdateMatchCountView
 
 urlpatterns = [
-    path('person/', person_list, name='person_list'),
+    path('', person_list, name='person_list'),
     path('person/add/', add_person, name='add_person'),
-   path('person/<int:person_id>/delete/', delete_person, name='delete_person'),
+    path('person/<int:person_id>/delete/', delete_person, name='delete_person'),
     path('api/update_match_count/<int:person_id>/', UpdateMatchCountView.as_view(), name='update_match_count'),
 ]
